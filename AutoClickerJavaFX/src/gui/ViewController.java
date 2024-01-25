9f0ebcf559e7ab0c3ad86f7a49aaf826e2a7c245
Cristhyan Couto <cristhyancouto@gmail.com>
2024-01-25 17:27:04 -0300

    Scene Created

---
+++ AutoClickerJavaFX/src/gui/ViewController.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ViewController {
+
+	//Text Fields Time Variables
+	@FXML
+	private TextField txtHours;
+	@FXML
+	private TextField txtMins;
+	@FXML
+	private TextField txtSecs;
+	@FXML
+	private TextField txtMilliseconds;
+}
