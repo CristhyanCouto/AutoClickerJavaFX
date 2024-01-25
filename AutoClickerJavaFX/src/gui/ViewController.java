1d878619df82e67fc775710d84ed245f21a75791
Cristhyan Couto <cristhyancouto@gmail.com>
2024-01-25 18:04:38 -0300

    Spinner Commit

--- AutoClickerJavaFX/src/gui/ViewController.java
+++ AutoClickerJavaFX/src/gui/ViewController.java
@@ -1,9 +1,15 @@
 package gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
 
-public class ViewController {
+public class ViewController implements Initializable {
 
 	//Text Fields Time Variables
 	@FXML
@@ -14,4 +20,19 @@ public class ViewController {
 	private TextField txtSecs;
 	@FXML
 	private TextField txtMilliseconds;
+	
+	//Spinner
+	@FXML
+	private Spinner<Integer> spinnerTimes;
+	int currentValueSpinner;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		SpinnerValueFactory<Integer> valueFactory = 
+				new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 1000);
+		
+		valueFactory.setValue(1);
+		spinnerTimes.setValueFactory(valueFactory);
+		currentValueSpinner = spinnerTimes.getValue();
+	}
 }
